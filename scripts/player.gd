e0e97bb5bbd91abc2a04eb6b3db41efacf4509b8
Oscar Carballal <oscar.carballal@protonmail.com>
2020-02-01 22:31:16 +0100

    Moar changessss!!

--- scripts/player.gd
+++ scripts/player.gd
@@ -76,3 +76,50 @@ func _physics_process(delta):
 		if JUMP:
 			print("JUMP")
 			set_axis_velocity(Vector2(0, -jump_force))
+
+
+
+
+extends KinematicBody2D
+
+export (int) var ACCELERATION = 512
+export (int) var MAX_SPEED = 64
+export (float) var FRICTION = 0.25
+export (int) var GRAVITY = 200
+export (int) var JUMP_FORCE = 128
+
+var motion = Vector2.ZERO
+
+func _physics_process(delta):
+var input_vector = get_input_vector()
+apply_horizontal_force(input_vector, delta)
+apply_friction(input_vector)
+jump_check()
+apply_gravity(delta)
+move()
+
+func get_input_vector():
+var input_vector = Vector2.ZERO
+input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+return input_vector
+
+func apply_horizontal_force(input_vector, delta):
+if input_vector.x != 0:
+motion.x += input_vector.x * ACCELERATION * delta
+motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+
+func apply_friction(input_vector):
+if input_vector.x == 0 and is_on_floor():
+motion.x = lerp(motion.x, 0, FRICTION)
+
+func jump_check():
+if is_on_floor():
+if Input.is_action_pressed("ui_up"):
+motion.y = -JUMP_FORCE
+func apply_gravity(delta):
+if not is_on_floor():
+motion.y += GRAVITY * delta
+motion.y = min(motion.y, JUMP_FORCE)
+
+func move():
+motion = move_and_slide(motion, Vector2.UP)
